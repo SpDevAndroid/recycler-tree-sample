b0702f830a5d3e93ab913d9e47e19ab8efe7d2a2
Shubham.Pundir <shubham.pundir@timesinternet.in>
2024-12-12 12:40:19 +0530

    Added new fragment EdgeToEdgeContentFragment Added code for edge to edge support in activity

--- app/src/main/java/com/tp/recyclertree/RecyclerTreeSampleActivity.kt
+++ app/src/main/java/com/tp/recyclertree/RecyclerTreeSampleActivity.kt
@@ -1,8 +1,14 @@
 package com.tp.recyclertree
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -15,16 +21,47 @@ class RecyclerTreeSampleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecyclerTreeSampleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         binding = ActivityRecyclerTreeSampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bottomNavigationOverlapHandling(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_recycler_tree_sample)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    /**
+     *  Making the Navigation system bar not overlapping with the activity
+     *  This method need to call after setContentView() of an activity
+     */
+    private fun bottomNavigationOverlapHandling(root: View) {
+        if (Build.VERSION.SDK_INT >= 30) {
+            // Root ViewGroup of my activity
+            ViewCompat.setOnApplyWindowInsetsListener(root) { view, windowInsets ->
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+                // Apply the insets as a margin to the view. Here the system is setting
+                // only the bottom, left, and right dimensions, but apply whichever insets are
+                // appropriate to your layout. You can also update the view padding
+                // if that's more appropriate.
+
+                view.layoutParams = (view.layoutParams as FrameLayout.LayoutParams).apply {
+                    leftMargin = insets.left
+                    bottomMargin = insets.bottom
+                    rightMargin = insets.right
+                }
+
+                // Return CONSUMED if you don't want want the window insets to keep being
+                // passed down to descendant views.
+                WindowInsetsCompat.CONSUMED
+            }
+
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
