832d056986205ccbb433eb3af5855faecfbb0882
Shubham.Pundir <shubham.pundir@timesinternet.in>
2024-12-20 11:54:19 +0530

    Jetpack compose testing sample added

---
+++ app/src/main/java/com/tp/recyclertree/jetpackComposeSample/ComposeSampleActivity.kt
@@ -0,0 +1,49 @@
+package com.tp.recyclertree.jetpackComposeSample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tp.recyclertree.jetpackComposeSample.ui.theme.ComposablePOCTheme
+
+class ComposeSampleActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposablePOCTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposablePOCTheme {
+        Greeting("Android")
+    }
+}
